cb8f5574d64d834c3d2d5cb2a347dc81467d7405
mturlaev <muhammadturlaev@gmail.com>
2022-07-25 14:55:59 +0300

    изменил название компонента

--- v2.0-search-work/src/components/MainRoutes/MainRoute.jsx
+++ v2.0-search-work/src/components/MainRoutes/MainRoute.jsx
@@ -6,7 +6,7 @@ import SearchPage from '../pages/SearchWorkPage/SearchPage';
 import VacanciesAddPage from '../pages/VacanciesAdd/VacanciesAddPage';
 import VacanciesPage from '../pages/VacanciesPage/VacanciesPage';
 
-const Main = () => {
+const MainRoute = () => {
     return (
         <div>
             <Routes>
@@ -20,4 +20,4 @@ const Main = () => {
     );
 };
 
-export default Main;
+export default MainRoute;
