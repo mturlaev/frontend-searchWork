b740d68c2bb67a37e01bedded1707a7c4993a6d2
Israil <cosynap@mail.ru>
2022-07-25 16:04:11 +0300

    объединил компоненты: добавил хидер, роуты, страницы

--- v2.0-search-work/src/components/navbar/mainNavbar/pages/HomePage/HomePage.jsx
+++ v2.0-search-work/src/components/navbar/mainNavbar/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import App from '../../../../../App';
 import Main from '../../../../main/Main';
+import Navbar from '../../../Navbar';
 
 const HomePage = () => {
     return (
         <div>
+            <Navbar/>
              <Main/>
         </div>
     );
