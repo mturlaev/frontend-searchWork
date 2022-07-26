efafe9bba3899e27d1704f014272f10286bcf6d1
Magomed-Emin <sadikov.magomed@bk.ru>
2022-07-26 22:39:31 +0300

    прописал логику авторизации на front

--- v2.0-search-work/src/components/MainRoutes/MainRoute.jsx
+++ v2.0-search-work/src/components/MainRoutes/MainRoute.jsx
@@ -6,6 +6,9 @@ import ResumePage from '../navbar/mainNavbar/pages/ResumePage/ResumePage';
 import SearchPage from '../navbar/mainNavbar/pages/SearchWorkPage/SearchPage';
 import VacanciesAddPage from '../navbar/mainNavbar/pages/VacanciesAdd/VacanciesAddPage';
 import VacanciesPage from '../navbar/mainNavbar/pages/VacanciesPage/VacanciesPage';
+import SignUp from '../RegAuth/SignUp';
+import SignIn from '../RegAuth/SignIn';
+import PreSignIn from '../RegAuth/PreSignIn';
 
 const MainRoute = () => {
     return (
@@ -16,10 +19,14 @@ const MainRoute = () => {
                 <Route path='/search/vacancy' element={<SearchPage/>} />
                 <Route path='/vacancy' element={<VacanciesAddPage/>} />
                 <Route path='/catalog/vacancies' element={<VacanciesPage/>} />
-                <Route path="/" element={<HomePage />}/ >
+                <Route path='/signUp' element={<SignUp />}/>
+                <Route path='/signIn' element={<SignIn />}/>
+                <Route path="/" element={<HomePage />} />
+                <Route path='/preSignIn' element={<PreSignIn />} />
             </Routes>
         </div>
     );
 };
+
 
 export default MainRoute;
---
+++ v2.0-search-work/src/components/feauters/userSlice.js
@@ -0,0 +1,107 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  signingUp: false,
+  signingIn: false,
+  error: null,
+  loading: false,
+  user: localStorage.getItem("user"),
+  token: localStorage.getItem("token"),
+  email: localStorage.getItem("email")
+};
+
+export const registration = createAsyncThunk(
+  "user/registration",
+  async ({email, password}, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:4000/registration", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({email, password})
+      });
+
+      const data = await res.json();
+
+      if (data.error) {
+        return thunkAPI.rejectWithValue(data.error);
+      } else {
+        return thunkAPI.fulfillWithValue(data);
+      }
+    } catch (error) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+
+export const login = createAsyncThunk(
+  "user/login",
+  async ({email, password}, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:4000/login", {
+        method: "POST",
+        headers: {"Content-Type": "application/json" },
+        body: JSON.stringify({email, password})
+      });
+      const data = await res.json();
+      console.log(data)
+      if (data.error) {
+        return thunkAPI.rejectWithValue(data.error);
+      } else {
+        localStorage.setItem("token", data.accessToken)
+        return thunkAPI.fulfillWithValue(data);
+      }
+    } catch (error) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+)
+
+export const logout = createAsyncThunk(
+  "user/logout",
+  async (_, thunkAPI) => {
+    localStorage.clear()
+  }
+)
+
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+    .addCase(registration.fulfilled, (state, action) => {
+      state.signingUp = false;
+      state.error = null;
+    })
+    .addCase(registration.rejected, (state, action) => {
+      state.signingUp = false;
+      state.error = "Такой пользователь уже существует";
+    })
+
+    .addCase(login.fulfilled, (state, action) => {
+      state.signingIn = false;
+      state.error = null;
+      state.token = action.payload.token;
+      state.user = action.payload.user;
+      state.email = action.payload.email;
+    })
+    .addCase(login.rejected, (state, action) => {
+      state.error = action.payload
+    })
+
+    .addCase(logout.fulfilled, (state, action) => {
+      state.signingIn = null;
+      state.error = null;
+      state.token = null;
+      state.user = null;
+      state.email = null;
+    })
+    .addCase(logout.rejected, (state, action) => {
+      state.error = "Ошибка при выходе";
+    })
+  }
+})
+
+export default userSlice.reducer
